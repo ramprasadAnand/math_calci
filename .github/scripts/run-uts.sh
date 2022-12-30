bad7c296b175f6122483f978c956dcc600be372e
Ramprasad A <ramprasad.a@razorpay.com>
2022-12-30 15:21:53 +0530

    Script fix

--- .github/scripts/run-uts.sh
+++ .github/scripts/run-uts.sh
@@ -2,8 +2,6 @@
 
 set -eo pipefail
 
-cd RazorpayIOS/
-
 overallUt="939:     Executed 97 tests, with 0 failures (0 unexpected) in 0.140 (0.160) seconds"
 trimPrefix= exec overallUt  | cut -d " " -f 9-
 trimSuffix=${{trimPrefix | cut -f1 -d"("}}
