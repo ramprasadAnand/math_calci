411923b0320e3256c343bad1b08d20b805377369
Ramprasad A <ramprasad.a@razorpay.com>
2022-12-30 15:35:08 +0530

    Text script fix 7

--- .github/scripts/run-uts.sh
+++ .github/scripts/run-uts.sh
@@ -2,6 +2,7 @@
 
 set -eo pipefail
 
-trimPrefix = $("939:     Executed 97 tests, with 0 failures (0 unexpected) in 0.140 (0.160) seconds" | cut -d " " -f 9-)
-trimSuffix = $($trimPrefix | cut -f1 -d"(")
+overallUt = "939:     Executed 97 tests, with 0 failures (0 unexpected) in 0.140 (0.160) seconds"
+trimPrefix = echo $overallUt  | cut -d " " -f 9-
+trimSuffix = echo $trimPrefix | cut -f1 -d"("
 echo $trimSuffix
