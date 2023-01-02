131be176ba72047d70bff7261b0e7a1c3996a2f3
Ramprasad A <ramprasad.a@razorpay.com>
2023-01-02 13:40:22 +0530

    Test passing path to access the data file

--- .github/scripts/run-uts.sh
+++ .github/scripts/run-uts.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-overallUt=`grep -n "Executed" ./log_results/xcodebuild.log | tail -1`
+overallUt=`grep -n "Executed" "$1" | tail -1`
 trimPrefix=`echo $overallUt  | cut -d " " -f 2-`
 trimSuffix=`echo $trimPrefix | cut -f1 -d"("`
 echo $trimSuffix
