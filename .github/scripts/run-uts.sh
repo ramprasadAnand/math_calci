e401247bb91aa97afd4d72d30bcf51b09914d761
Ramprasad A <ramprasad.a@razorpay.com>
2023-01-02 11:36:25 +0530

    Script path error fix

--- .github/scripts/run-uts.sh
+++ .github/scripts/run-uts.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-overallUt=`grep -n "Executed" ./xcodebuild.log`
+overallUt=`grep -n "Executed" ./log_results/xcodebuild.log`
 trimPrefix=`echo $overallUt  | cut -d " " -f 2-`
 trimSuffix=`echo $trimPrefix | cut -f1 -d"("`
 echo $trimSuffix
