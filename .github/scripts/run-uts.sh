ecd5c1754f33ac7ace289164a4a48eae0c063ce2
Ramprasad A <ramprasad.a@razorpay.com>
2023-01-02 11:34:15 +0530

    Reading the xcodebuild log from log file

--- .github/scripts/run-uts.sh
+++ .github/scripts/run-uts.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-overallUt="939:     Executed 97 tests, with 0 failures (0 unexpected) in 0.140 (0.160) seconds"
+overallUt=`grep -n "Executed" ./xcodebuild.log`
 trimPrefix=`echo $overallUt  | cut -d " " -f 2-`
 trimSuffix=`echo $trimPrefix | cut -f1 -d"("`
 echo $trimSuffix
